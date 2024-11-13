d53411df16a2dba848eee6465abfe032ef0e50b1
mborne <mborne@users.noreply.github.com>
2024-11-13 23:19:54 +0100

    retouche du point de départ

--- src/Point.ts
+++ src/Point.ts
@@ -1,18 +1,18 @@
 import Coordinate from "./Coordinate";
 
 export default class Point {
-  private coordinates: Coordinate;
+  private coordinates?: Coordinate;
 
   constructor(coordinate?: Coordinate) {
-    this.coordinates = coordinate || [];
+    this.coordinates = coordinate ;
   }
 
   x(): number {
-    return this.coordinates.length < 1 ? Number.NaN : this.coordinates[0];
+    return this.coordinates ? this.coordinates[0] : Number.NaN ;
   }
 
   y(): number {
-    return this.coordinates.length < 2 ? Number.NaN : this.coordinates[1];
+    return this.coordinates ? this.coordinates[1] : Number.NaN ;
   }
 
 }
